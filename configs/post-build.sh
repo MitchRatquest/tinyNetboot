56531d0d73c2e0a5c1e4756690446dcf15c5a2ec
Mitchell Dokken <mitchell.dokken@gmail.com>
2017-09-29 11:00:32 -0500

    directory creation issue

--- configs/post-build.sh
+++ configs/post-build.sh
@@ -9,16 +9,11 @@ BOOT_CMD="${BOARD_DIR}/boot.cmd"
 BOOT_CMD_H="${BINARIES_DIR}/boot.scr"
 
 #copy configuration files to image
-#cp $BOARD_DIR/S45ntpdate $TARGET_DIR/etc/init.d
-#cp $BOARD_DIR/asound.conf $TARGET_DIR/etc
-cp $BOARD_DIR/interfaces $TARGET_DIR/etc/network
-#cp $BOARD_DIR/sun8i-h3-nanopi-neo.dtb $IMAGE_DIR/sun8i-h3-nanopi-neo.dtb
-#cp $BOARD_DIR/dhcpd.conf $TARGET_DIR/etc/dhcp
-#cp $BOARD_DIR/S80dhcp-server $TARGET_DIR/etc/init.d
-#cp $BOARD_DIR/S41usb $TARGET_DIR/etc/init.d
-#ln -s $TARGET_DIR/usr/bin/wish8.6 $TARGET_DIR/usr/bin/wish
-mv $TARGET_DIR/etc/init.d/S40network $TARGET_DIR/etc/init.d/network-disabled
+mkdir -p $TARGET_DIR/var/www/data
+mkdir -p $TARGET_DIR/var/lib/tftpboot
 
+cp $BOARD_DIR/interfaces $TARGET_DIR/etc/network
+mv $TARGET_DIR/etc/init.d/S40network $TARGET_DIR/etc/init.d/network-disabled
 cp $BOARD_DIR/dnsmasq.conf $TARGET_DIR/etc
 cp $BOARD_DIR/S90pythonhttp $TARGET_DIR/etc/init.d
 cp -r $BOARD_DIR/netboot/* $TARGET_DIR/var/www/data
